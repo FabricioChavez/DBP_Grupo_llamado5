ad26e80a4c8de16256f84e2654a18a2ff4aeb827
RodrigoLeonUTEC <104233585+RodrigoLeonUTEC@users.noreply.github.com>
2023-06-15 19:45:02 -0500

    Rodrigo's first commit

--- tienda-app/src/DataComponents/mangalist.js
+++ tienda-app/src/DataComponents/mangalist.js
@@ -1,28 +1,85 @@
+import React, { useState } from "react";
+import styled from "styled-components";
 
-import React from "react";
+const MangaListContainer = styled.div`
+  display: grid;
+  grid-template-columns: repeat(3, 1fr);
+  grid-gap: 10px;
+`;
 
+const MangaItem = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  text-align: center;
+  transition: transform 0.3s ease-in-out;
+  cursor: pointer;
 
-function MangaList(props){
+  &:hover {
+    transform: scale(1.1);
+  }
+`;
 
-return(
-<div className="MangaList">
+const MangaImage = styled.img`
+  height: 500px;
+  width: 100%;
+  object-fit: cover;
+  border: 4px solid;
+  animation: colorAnimation 3s infinite;
 
-<h1> API flask proof </h1>
-{props.manga && props.manga.map(manga=>{
+  @keyframes colorAnimation {
+    0% { border-color: rgb(0,255,159); }
+    25% { border-color: rgb(0,184,255); }
+    50% { border-color: rgb(0,30,255); }
+    75% { border-color: rgb(189,0,255); }
+    100% { border-color: rgb(214,0,255); }
+  }
+`;
 
-    return (
-    <div key ={manga.id}>
-        <img src={manga.link} id={manga.id}/>
-    <h2>{manga.nombre}</h2>
-    </div>
+const MangaName = styled.h2`
+  color: white;
+`;
 
-    )
-})}
+function MangaList(props) {
+  const { manga } = props;
+  const [selectedManga, setSelectedManga] = useState(null);
 
+  const handleMouseEnter = (manga) => {
+    setSelectedManga(manga);
+  };
 
+  const handleMouseLeave = () => {
+    setSelectedManga(null);
+  };
 
-</div>
-);
+  return (
+    <MangaListContainer>
+      {manga.map((manga, index) => (
+        <MangaItem
+          key={manga.id}
+          style={{ gridRow: index > 2 ? "2" : "1", gridColumn: (index % 3) + 1 }}
+          onMouseEnter={() => handleMouseEnter(manga)}
+          onMouseLeave={handleMouseLeave}
+        >
+          <MangaImage
+            src={manga.link}
+            id={manga.id}
+            alt={manga.nombre}
+            style={{
+              border: selectedManga === manga ? "6px solid red" : "4px solid",
+            }}
+          />
+          <MangaName
+            style={{
+              transform: selectedManga === manga ? "scale(1.1)" : "scale(1)",
+            }}
+          >
+            {manga.nombre}
+          </MangaName>
+        </MangaItem>
+      ))}
+    </MangaListContainer>
+  );
 }
 
-export default MangaList
+export default MangaList;
--- tienda-app/src/Interfaz.js
+++ tienda-app/src/Interfaz.js
@@ -1,17 +1,18 @@
 import React from 'react';
 import './styles.css'; // Import the CSS file
 import 'bootstrap/dist/css/bootstrap.min.css';
+import Manga_fetch from './DataComponents/MangaFetcher';
 
 function Interfaz(props) {
   const cosa={
-      backgroundColor:'#A2B2EE'
+      backgroundColor:'#26292B'
   }
   return (
    
     <>
     <div style={cosa}> 
-    <nav className="navbar navbar-expand-lg " style={{ backgroundColor: '#2e3239' }}>
-        <a className="navbar-brand" style={{ color: '#A2B2EE' }} href="Pagina principal">Página principal</a>
+    <nav className="navbar navbar-expand-lg " style={{ backgroundColor: '#2e3239'}}>
+        <a className="navbar-brand" style={{ color: '#A2B2EE', fontFamily:'Quicksand'}} href="Pagina principal">Página principal</a>
         <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
           <span className="navbar-toggler-icon"></span>
         </button>
@@ -19,15 +20,15 @@ function Interfaz(props) {
         <div className="collapse navbar-collapse" id="navbarNav">
           <ul className="navbar-nav">
             <li className="nav-item active">
-              <a className="nav-link" style={{ color: '#A2B2EE' }} href="buscar">Interfaz</a>
+              <a className="nav-link" style={{ color: '#A2B2EE', fontFamily:'Quicksand'}} href="buscar">Interfaz</a>
             </li>
           </ul>
         </div>
       </nav>
       <div className="Cuadro">
         <div className="box">
-          <label htmlFor="categorias">Seleccione por categoría:</label>
-          <select id="categorias" name="categorias">
+          <label htmlFor="categorias" style={{ fontFamily: 'Quicksand', fontSize: '22px' }}>Seleccione por categoría:</label>
+          <select id="categorias" name="categorias" style={{ borderRadius: '15px' }}>
             <option value="no se">Seinen</option>
             <option value="no se">Shonen</option>
             <option value="no se">no se</option>
@@ -35,17 +36,20 @@ function Interfaz(props) {
           </select>
         </div>
       </div>
+
+
       <div className="Cuadro2">
         <div className="box">
           <div className="input">
-            <label>Buscar por nombre:</label>
-            <input type="text" id="Nombre"></input>
+          <label htmlFor='nombres' style={{fontFamily: 'Quicksand', fontSize: '20px'}}>Buscar por nombre:</label>
+            <input type="text" id="Nombre" style={{borderRadius: '15px'}}></input>
           </div>
         </div>
       </div>
-
-
+      <div style={{marginTop:'150px'}}><Manga_fetch></Manga_fetch></div>
       </div>
+      
+      
           </>
   );
 }
