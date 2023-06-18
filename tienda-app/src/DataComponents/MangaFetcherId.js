d37114e5ff4ed5ce836c601501b9bcbca2c99702
drypnotic <flavio.tipula@utec.edu.pe>
2023-06-17 19:31:37 -0500

    css id

--- tienda-app/src/DataComponents/MangaFetcherId.js
+++ tienda-app/src/DataComponents/MangaFetcherId.js
@@ -1,8 +1,9 @@
 import React, { useState, useEffect } from "react";
 import { useParams } from "react-router-dom";
+import './MangaId.css';
 
 function MangaFetcherId(props) {
-const {id}=props
+  const { id } = props;
   const [manga, setManga] = useState(null);
 
   useEffect(() => {
@@ -17,16 +18,19 @@ const {id}=props
   }
 
   return (
-    <div className="MangaInfo">
-      <h1>Información de {manga.nombre}</h1>
-      <div>
-        <img src={manga.link} alt={manga.nombre} />
-        <h2>Nombre: {manga.nombre}</h2>
-        <h2>Edición: {manga.edicion}</h2>
-        <h2>Stock: {manga.cant_stock}</h2>
-        <h2>Género: {manga.genero}</h2>
-        <h2>Precio: {manga.precio}</h2>
-        <button className="btn btn-primary">Comprar</button>
+    <div>
+      <h1  className="p">Información de {manga.nombre}</h1>
+      <div className="MangaInfo">
+      <img className="Imagen" src={manga.link} alt={manga.nombre} />
+        <h2 className="Texto">Nombre: <strong>{manga.nombre}</strong></h2>
+        <h2 className="Texto3">Edición: {manga.edicion}</h2>
+        <h2 className="Texto4">Stock: {manga.cant_stock}</h2>
+        <h2 className="Texto5"><em>Género: {manga.genero}</em></h2>
+        <h2 className="Texto6"> ${manga.precio}</h2>
+        <div className="rectangulo"></div>
+        <div className="rectangulo2"></div>
+        <button className="btn btn-primary Texto2">Comprar</button>
+        
       </div>
     </div>
   );
