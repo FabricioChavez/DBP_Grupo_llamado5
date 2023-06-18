56567f363af70c2d6db153ba0903aa9efe59d8b8
drypnotic <flavio.tipula@utec.edu.pe>
2023-06-17 20:23:42 -0500

    ahora si

---
+++ tienda-app/src/MangaFetcherId.js
@@ -0,0 +1,39 @@
+import React, { useState, useEffect } from "react";
+import { useParams } from "react-router-dom";
+import './MangaId.css';
+
+function MangaFetcherId(props) {
+  const { id } = props;
+  const [manga, setManga] = useState(null);
+
+  useEffect(() => {
+    fetch(`http://127.0.0.1:5000/manga/${id}`)
+      .then(resp => resp.json())
+      .then(data => setManga(data))
+      .catch(error => console.log(error));
+  }, [id]);
+
+  if (!manga) {
+    return <div>Cargando...</div>;
+  }
+
+  return (
+    <div>
+      <h1  className="p">Información de {manga.nombre}</h1>
+      <div className="MangaInfo">
+      <img className="Imagen" src={manga.link} alt={manga.nombre} />
+        <h2 className="Texto">Nombre: <strong>{manga.nombre}</strong></h2>
+        <h2 className="Texto3">Edición: {manga.edicion}</h2>
+        <h2 className="Texto4">Stock: {manga.cant_stock}</h2>
+        <h2 className="Texto5"><em>Género: {manga.genero}</em></h2>
+        <h2 className="Texto6"> ${manga.precio}</h2>
+        <div className="rectangulo"></div>
+        <div className="rectangulo2"></div>
+        <button    className="btn btn-primary Texto2">Comprar</button>
+        
+      </div>
+    </div>
+  );
+}
+
+export default MangaFetcherId;
